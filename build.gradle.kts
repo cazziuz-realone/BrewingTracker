34b335df27bc2d9971ad43f833b94ed3e236f3d4
cazziuz-realone <argotzcreations@gmail.com>
2025-07-20 13:32:03 -0400

    Fix root build.gradle.kts - Add repositories and proper configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,25 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("kotlin-kapt") apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
