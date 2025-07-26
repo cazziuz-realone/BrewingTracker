c2faf49701234512bc1dfd2cc99701612ab531ef
Cazziuz <argotzcreations@gmail.com>
2025-07-26 02:42:53 -0400

    Remove duplicate RecipeLibraryViewModel.kt file to fix class redeclaration error

--- app/src/main/java/com/brewingtracker/presentation/viewmodel/RecipeLibraryViewModel.kt
+++
@@ -1,6 +0,0 @@
-// THIS FILE IS A DUPLICATE AND SHOULD BE DELETED
-// The correct RecipeLibraryViewModel is located at:
-// app/src/main/java/com/brewingtracker/presentation/screens/recipe/RecipeLibraryViewModel.kt
-//
-// This duplicate file causes redeclaration errors and needs to be removed.
-// It was mistakenly placed in the wrong directory with wrong package declaration.
